0f7fc1781a67aab59a8dc38250ab5e2ffc6d5ca7
Roberto Gazia <r.gazia@arduino.cc>
2026-02-18 18:33:56 +0100

    Fix brick folder name (#97)

--- src/arduino/app_bricks/hand_gesture_tracking/examples/02_enter_exit.py
+++ src/arduino/app_bricks/hand_gesture_tracking/examples/02_enter_exit.py
@@ -5,13 +5,10 @@
 # EXAMPLE_NAME = "Hand enter/exit detection"
 # EXAMPLE_REQUIRES = "Requires a connected camera"
 
-from arduino.app_bricks.hand_gesture_detection import HandGestureTracking
-from arduino.app_peripherals.camera.websocket_camera import WebSocketCamera
+from arduino.app_bricks.hand_gesture_tracking import HandGestureTracking
 from arduino.app_utils.app import App
 
-camera = WebSocketCamera()
-camera.start()
-pd = HandGestureTracking(camera)
+pd = HandGestureTracking()
 pd.on_enter(lambda: print("Hi there!"))
 pd.on_exit(lambda: print("Goodbye!"))
 
--- src/arduino/app_bricks/hand_gesture_tracking/examples/03_confirm_deny.py
+++ src/arduino/app_bricks/hand_gesture_tracking/examples/03_confirm_deny.py
@@ -5,13 +5,10 @@
 # EXAMPLE_NAME = "Get user confirmation with hand gestures"
 # EXAMPLE_REQUIRES = "Requires a connected camera"
 
-from arduino.app_bricks.hand_gesture_detection import HandGestureTracking
-from arduino.app_peripherals.camera.websocket_camera import WebSocketCamera
+from arduino.app_bricks.hand_gesture_tracking import HandGestureTracking
 from arduino.app_utils.app import App
 
-camera = WebSocketCamera()
-camera.start()
-pd = HandGestureTracking(camera)
+pd = HandGestureTracking()
 pd.on_gesture("Thumb_Up", lambda meta: print("Operation confirmed!"))
 pd.on_gesture("Thumb_Down", lambda meta: print("Operation denied!"))
 
