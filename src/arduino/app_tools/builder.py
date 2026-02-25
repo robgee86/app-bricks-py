cdaafeedd0e02ca9e9abda130993de21b5a115b6
Roberto Gazia <r.gazia@arduino.cc>
2026-02-25 16:29:04 +0100

    WebUI: add CORS support (#100)
    * REVERTME: use specific tag for this branch
    
    * feat(web_ui): add CORS support
    
    * fix: fmt

--- src/arduino/app_tools/builder.py
+++ src/arduino/app_tools/builder.py
@@ -15,7 +15,7 @@ import shutil
 def run_preprocessing(dev_mode: bool = False) -> None:
     registry = os.getenv("PUBLIC_IMAGE_REGISTRY_BASE", None)
     if dev_mode:
-        version = "dev-latest"
+        version = "dev-next"
     else:
         version = get_version(
             version_scheme="only-version",
