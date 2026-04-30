01dcc184b7cad04b76676a124f3e492fc3acce10
Roberto Gazia <r.gazia@arduino.cc>
2026-04-30 16:56:21 +0200

    refactor(ci): switch to using version.py for dynamic version resolution

---
+++ src/arduino/version.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: Copyright (C) ARDUINO SRL (http://www.arduino.cc)
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from __future__ import annotations
+
+__all__ = [
+    "__version__",
+    "version",
+]
+
+__version__: str = "0.0.0"
+version: str = __version__
