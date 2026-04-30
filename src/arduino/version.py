e8943b4e0008ef2927c93f317549bfcd90eb95f9
Roberto Gazia <r.gazia@arduino.cc>
2026-04-30 19:51:59 +0200

    feat(ci): migrate build process from 'builder' custom backend to standard 'build_meta' backend

--- src/arduino/version.py
+++ src/arduino/version.py
@@ -2,12 +2,4 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-from __future__ import annotations
-
-__all__ = [
-    "__version__",
-    "version",
-]
-
 __version__: str = "0.0.0"
-version: str = __version__
